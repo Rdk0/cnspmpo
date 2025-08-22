dfc07150b98071d670bc476b8f673b483669b5bd
Rdk0 <radeklaufer@gmail.com>
2025-08-22 14:54:31 -0400

    Add files via upload

--- src/pmpo/core.clj
+++ src/pmpo/core.clj
@@ -2,7 +2,8 @@
   (:require
    [tablecloth.api :as tc]
    [pmpo.read-data :as rd]
-   [cli-matic.core :refer [run-cmd]])
+   [cli-matic.core :refer [run-cmd]]
+   [portal.api :as p])
   (:gen-class))
 
 (defn round-number
@@ -12,13 +13,13 @@
     (/ (Math/round (* num factor)) factor)))
 
 
-(defn app [input-file output-file]
-  (let [results-file output-file; "data/results-table.csv" ; output file
-        input-file   input-file ;"data/input.csv" ; input file
+(defn app [params] ;input-file output-file]
+  (let [results-file (:filename-out params) ;output-file; "data/results-table.csv" ; output file
+        input-file   (:filenme-in params) ;input-file ;"data/input.csv" ; input file
         float-precision 2 ; number of decimal places in the output
         input-file-path (-> (java.io.File. input-file) .getAbsolutePath)
         output-file-path (-> (java.io.File. results-file) .getAbsolutePath)
-        result-table (tc/select-columns (rd/report-results)  [:ID :cns-mpo-calc :cns-mpo-sigm-calc])
+        result-table (tc/select-columns (rd/report-results input-file)  [:ID :cns-mpo-calc :cns-mpo-sigm-calc])
         result-table (-> result-table
                          (tc/map-columns :cns-mpo-calc      (fn [val] (round-number float-precision val))) ; Round to 2 decimal places
                          (tc/map-columns :cns-mpo-sigm-calc (fn [val] (round-number float-precision val))))]
@@ -34,28 +35,33 @@
           :description "calculations of pMPO.  An input csv file is required with the following columns
                       MW, HBD, TPSA, cLogD_ACD_v15, and mbpKa"
           :version "0.0.1"
-          :runs app
+          :runs app 
           :opts [{:option "filename-in"
                   :short "in"
                   :as "input file"
-                  :default "input.csv"
+                  :default "./data/input.csv"
                   :type :string}
                  {:option "filename-out"
                   :short "out"
-                  :as "output file"
-                  :default "results-table.csv"
+                  :as "./data/output file"
+                  :default "results-table.csv"   
                   :type :string}]}})
-
-
 
 
 (defn -main
   [& args]
+  (println "recieved args: " args)
   (run-cmd args CONFIGURATION))
+;)
 
 
 (comment
   ;clj -M -m pmpo.core --filename-in my-data.csv --filename-out results.csv
-  )
+  (pmpo.core/-main "--filename-in" "./data/input.csv" "--filename-out" "./data/results.csv")
+(def portal (p/open))
+(add-tap #'p/submit)
+  (tap> :hello)
+  (tap> CONFIGURATION)
+  (-main )
+)
   
-
--- src/pmpo/read_data.clj
+++ src/pmpo/read_data.clj
@@ -4,16 +4,19 @@
    [pmpo.model :as md]))
 
 
-(def new-params
-  "read in the values of ID, MW, HBD, TPSA, cLogD_ACD_v15, and mbpKa. "
-  (let [params (tc/dataset ".\\data\\input.csv")]
+(defn new-params
+  "read in the values of ID, MW, HBD, TPSA, cLogD_ACD_v15, and mbpKa. from the csv file 
+   return dataframe"
+  [filename-in]
+  (let [params (tc/dataset filename-in)];".\\data\\input.csv")]
     (-> params
         (tc/rename-columns
          (into {} (map (fn [col-name] [col-name (keyword col-name)])
                        (tc/column-names params)))))))
 
 (defn calculate-result
-  "Calculates the result based on MW, HBD, TPSA, cLogD_ACD_v15, and mbpKa. apply sigmoid correction?"
+  "Calculates the combined pmpo a single value based on MW, HBD, TPSA, cLogD_ACD_v15, and mbpKa. apply sigmoid correction
+  return sum on contributions as a float value for a row"
   [{:keys [MW HBD TPSA cLogD_ACD_v15 mbpKa]} sigmoid-correction?]
   (+ (md/calculate-contribution :mw MW sigmoid-correction?)
      (md/calculate-contribution :hbd HBD sigmoid-correction?)
@@ -22,9 +25,9 @@
      (md/calculate-contribution :pka mbpKa sigmoid-correction?)))
 
 (defn calulated-pmpo-values
-  "create df with the calculated pmpo"
-  [sigmoid-correction?]
-  (let [new-params-small (tc/select-columns new-params [:ID :MW :HBD :TPSA :cLogD_ACD_v15 :mbpKa])
+  "create a dataframe with the calculated pmpo values by mapping each row of a dataframe with all the input values"
+  [sigmoid-correction? filename-in]
+  (let [new-params-small (tc/select-columns ( new-params filename-in) [:ID :MW :HBD :TPSA :cLogD_ACD_v15 :mbpKa])
         new (tc/drop-missing new-params-small)
         with-results  (tc/map-rows new
                                    (fn [row]
@@ -36,9 +39,10 @@
       with-results)))
 
 (defn report-results
-  "combine into a df all the calculated values"
-  []
-  (let [calculated-pmpo-no-sigmoid (calulated-pmpo-values false)
-        calculated-pmpo-sigmoid (calulated-pmpo-values true)]
+  "calcualte two separate dataframes - with and without sigmoid corrections -  then 
+   return a dataframe being a joint of the two dataframes"
+  [filename-in]
+  (let [calculated-pmpo-no-sigmoid (calulated-pmpo-values false filename-in)
+        calculated-pmpo-sigmoid (calulated-pmpo-values true filename-in)]
     (tc/left-join  calculated-pmpo-no-sigmoid calculated-pmpo-sigmoid :ID)))
 
