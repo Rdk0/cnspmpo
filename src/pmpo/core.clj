745c93676652563306dbb170703dc515a8ec3775
Rdk0 <radeklaufer@gmail.com>
2025-08-23 21:19:18 -0400

    Add files via upload

--- src/pmpo/core.clj
+++ src/pmpo/core.clj
@@ -2,9 +2,9 @@
   (:require
    [tablecloth.api :as tc]
    [pmpo.read-data :as rd]
-   [cli-matic.core :refer [run-cmd]]
-   [portal.api :as p])
+   [cli-matic.core :refer [run-cmd]])
   (:gen-class))
+
 
 (defn round-number
   " clean up float number to a specific precision"
@@ -13,9 +13,9 @@
     (/ (Math/round (* num factor)) factor)))
 
 
-(defn app [params] ;input-file output-file]
-  (let [results-file (:filename-out params) ;output-file; "data/results-table.csv" ; output file
-        input-file   (:filenme-in params) ;input-file ;"data/input.csv" ; input file
+(defn app [{:keys [i o]}] ;input-file output-file]
+  (let [results-file o  ;output-file; "data/results-table.csv" ; output file
+        input-file   i ;input-file ;"data/input.csv" ; input file
         float-precision 2 ; number of decimal places in the output
         input-file-path (-> (java.io.File. input-file) .getAbsolutePath)
         output-file-path (-> (java.io.File. results-file) .getAbsolutePath)
@@ -31,37 +31,25 @@
 
 
 (def CONFIGURATION
-  {:app  {:command "pmpo"
-          :description "calculations of pMPO.  An input csv file is required with the following columns
+   {:command "pmpo"
+          :description "calculations of cns-mpo values based on 
                       MW, HBD, TPSA, cLogD_ACD_v15, and mbpKa"
           :version "0.0.1"
           :runs app 
-          :opts [{:option "filename-in"
-                  :short "in"
+          :opts [{:option "i"
                   :as "input file"
                   :default "./data/input.csv"
                   :type :string}
-                 {:option "filename-out"
-                  :short "out"
-                  :as "./data/output file"
-                  :default "results-table.csv"   
-                  :type :string}]}})
+                 {:option "o"
+                  :as "output file"
+                  :default "./data/results-table.csv"   
+                  :type :string}]})
 
 
 (defn -main
   [& args]
   (println "recieved args: " args)
   (run-cmd args CONFIGURATION))
-;)
 
 
-(comment
-  ;clj -M -m pmpo.core --filename-in my-data.csv --filename-out results.csv
-  (pmpo.core/-main "--filename-in" "./data/input.csv" "--filename-out" "./data/results.csv")
-(def portal (p/open))
-(add-tap #'p/submit)
-  (tap> :hello)
-  (tap> CONFIGURATION)
-  (-main )
-)
   
